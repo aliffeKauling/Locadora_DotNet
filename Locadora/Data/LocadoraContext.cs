4a65e96c53789822fbe9b84b875e55e82665eb22
aliffeKauling <aliffe.kauling.aiesec@gmail.com>
2021-10-26 16:28:44 -0200

    First commit

---
+++ Locadora/Data/LocadoraContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Locadora.Model;
+
+namespace Locadora.Data
+{
+    public class LocadoraContext : DbContext
+    {
+        public LocadoraContext (DbContextOptions<LocadoraContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Locadora.Model.Movie> Movie { get; set; }
+    }
+}
---
+++ Locadora/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Locadora.Data;
+using Locadora.Model;
+
+namespace Locadora.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Locadora.Data.LocadoraContext _context;
+
+        public IndexModel(Locadora.Data.LocadoraContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
